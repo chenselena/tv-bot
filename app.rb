91ca5a230274081ed4c84c4331ec2f9712888a5a
Selena Chen <selena.chen@fullscript.com>
2019-10-30 13:43:27 -0400

    aaa

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ require_relative 'app/slack_authorizer'
 
 use SlackAuthorizer
 
+FIREWORK_CONGRATS = "Congratulations to %s for their first deploy!".freeze
+INVALID_RESPONSE = "Sorry! It doesn't look like that's a valid video."
+
 post '/slack/command' do
-  "OK"
+  case params['text'].to_s.strip
+  when 'firework', '' then FIREWORK_CONGRATS
+  else INVALID_RESPONSE
+  end
 end

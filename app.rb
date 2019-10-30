66e128f3c8c9ffbb70b463b7b36c2142d26da387
Selena Chen <selena.chen@fullscript.com>
2019-10-30 14:06:02 -0400

    idk if this will work

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@
 
 require 'sinatra'
 require_relative 'app/slack_authorizer'
+require_relative 'app/add_request_to_queue'
 
 use SlackAuthorizer
+use AddRequestToQueue
 
-FIREWORK_CONGRATS = "Congratulations to %s for their first deploy!".freeze
+FIREWORKS_RESPONSE = "Congratulations to %s for their first deploy!".freeze
 INVALID_RESPONSE = "Sorry! It doesn't look like that's a valid video."
+HELP_RESPONSE = "Use /tvplay to play a video on the screens. Example: /tvplay fireworks"
 
 post '/slack/command' do
   case params['text'].to_s.strip
-  when 'firework', '' then FIREWORK_CONGRATS
+  when 'help', '' then HELP_RESPONSE
+  when 'firework'
+    FIREWORKS_RESPONSE
+    AddRequestToQueue.new.call("firework")
   else INVALID_RESPONSE
   end
 end
--- app/slack_authorizer.rb
+++ app/slack_authorizer.rb
@@ -3,9 +3,11 @@
 class SlackAuthorizer
   UNAUTHORIZED_MESSAGE = 'Oops! This application is not authorized! Please review the token configuration.'.freeze
   UNAUTHORIZED_RESPONSE = ['200', {'Content-Type' => 'text'}, [UNAUTHORIZED_MESSAGE]]
+
   def initialize(app)
     @app = app
   end
+
   def call(env)
     req = Rack::Request.new(env)
     if req.params['token'] == ENV['SLACK_TOKEN']

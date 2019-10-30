d94a9e34966e085f060905b467632ad54ff0cac5
Selena Chen <selena.chen@fullscript.com>
2019-10-30 19:43:05 -0400

    rickroll

--- app.rb
+++ app.rb
@@ -7,20 +7,22 @@ require_relative 'services/add_request_to_queue'
 
 use SlackAuthorizer
 
-FIREWORKS_RESPONSE = "Congratulations to %s for their first deploy!".freeze
+FIRST_DEPLOY_MESSAGE = "Congratulations on your first deploy!!!!"
 INVALID_RESPONSE = "Sorry! It doesn't look like that's a valid video."
 HELP_RESPONSE = "Use /tvplay to play a video on the screens. Example: /tvplay fireworks"
 
 get '/firework' do
-  AddRequestToQueue.new.call("firework")
+  AddRequestToQueue.new.call("fireworks")
 end
 
 post '/slack/command' do
-  AddRequestToQueue.new.call("firework")
-
   case params['text'].to_s.strip
   when 'help', '' then HELP_RESPONSE
-  when 'firework', '' then FIREWORKS_RESPONSE
+  when 'firework'
+    FIRST_DEPLOY_MESSAGE  
+    AddRequestToQueue.new.call("fireworks")
+  when 'rickroll'
+    AddRequestToQueue.new.call("rickroll")
   else INVALID_RESPONSE
   end
 end

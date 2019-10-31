cafc8aebe2f8d65094e55383e3e4d9ad14da26fb
Selena Chen <selena.chen@fullscript.com>
2019-10-31 10:26:27 -0400

    maybe rickroll will work

--- app.rb
+++ app.rb
@@ -10,16 +10,24 @@ use SlackAuthorizer
 FIRST_DEPLOY_MESSAGE = "Congratulations on your first deploy!!!!"
 INVALID_RESPONSE = "Sorry! It doesn't look like that's a valid video."
 HELP_RESPONSE = "Use /tvplay to play a video on the screens. Example: /tvplay fireworks"
+RICKROLL = "you just got rickrolled xd"
 
 get '/firework' do
   AddRequestToQueue.new.call("fireworks")
 end
 
+get '/rickroll' do
+  AddRequestToQueue.new.call("rickroll")
+end
+
 post '/slack/command' do
   AddRequestToQueue.new.call("fireworks")
+  AddRequestToQueue.new.call("rickroll")
+
   case params['text'].to_s.strip
   when 'help', '' then HELP_RESPONSE
   when 'firework', '' then FIRST_DEPLOY_MESSAGE  
+  when 'rickroll', '' then RICKROLL
   else INVALID_RESPONSE
   end
 end

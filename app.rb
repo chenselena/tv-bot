eb07ee0f4c3659251d7c59108ee9a3d85c34c986
Karim Chukfeh <karimchukfeh@gmail.com>
2019-11-05 12:43:08 -0500

    route clean up

--- app.rb
+++ app.rb
@@ -7,26 +7,30 @@ require_relative 'services/add_request_to_queue'
 
 use SlackAuthorizer
 
-FIRST_DEPLOY_MESSAGE = "Congratulations on your first deploy!!!!"
+FIRST_DEPLOY_MESSAGE = 'Congratulations on your first deploy!!!!'
 INVALID_RESPONSE = "Sorry! It doesn't look like that's a valid command."
-HELP_RESPONSE = "Use /tvplay to play a video on the screens. Example: /tvplay fireworks"
-RICKROLL = "you just got rickrolled xd"
+HELP_RESPONSE = 'Use /tvplay to play a video on the screens. Example: /tvplay fireworks'
+RICKROLL = 'Never gonna give them up!'
 
 get '/fireworks' do
-  AddRequestToQueue.new.call("fireworks")
+  AddRequestToQueue.new.call('fireworks')
 end
 
 get '/rickroll' do
-  AddRequestToQueue.new.call("rickroll")
+  AddRequestToQueue.new.call('rickroll')
 end
 
 post '/slack/command' do
-  redirect '/fireworks'
-
   case params['text'].to_s.strip
-  when 'help', '' then HELP_RESPONSE
-  when 'fireworks', '' then FIRST_DEPLOY_MESSAGE
-  when 'rickroll', '' then RICKROLL
-  else INVALID_RESPONSE
+  when 'help'
+    HELP_RESPONSE
+  when 'fireworks'
+    AddRequestToQueue.new.call('fireworks')
+    FIRST_DEPLOY_MESSAGE
+  when 'rickroll'
+    AddRequestToQueue.new.call('rickroll')
+    RICKROLL
+  else
+    INVALID_RESPONSE
   end
 end
